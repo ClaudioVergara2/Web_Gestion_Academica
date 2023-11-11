cfb81696c5ba23aece8a05b28a2b749bfd7abdff
ClaudioVergara2 <claudio.vergara2@virginiogomez.cl>
2023-11-10 23:45:59 -0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApiNotas.Models;
+
+namespace WebApiNotas.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Asignatura()
+        {
+            return View();
+        }
+
+        public IActionResult Semestre()
+        {
+            return View();
+        }
+
+        public IActionResult IngresoAsignatura()
+        {
+            return View();
+        }
+
+        public IActionResult IngresoCurso()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
